fdb644d921eb6611d6d8d2a7ba5ab5c15851b19e
dean <wiebedean@gmail.com>
2021-05-19 21:10:01 -0600

    started work on search feature

--- themes/deafsask/footer.php
+++ themes/deafsask/footer.php
@@ -1,6 +1,11 @@
 <footer>
     <?php get_template_part('social_media'); ?>
     <a href="http://deanwiebe.ca/"><p> www.deanwiebe.ca &copy 2021</p></a>
+    <div class="search-overlay">
+        <form action="">
+            <input class="search-input" type="text">
+        </form>
+    </div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- themes/deafsask/js/scripts.js
+++ themes/deafsask/js/scripts.js
@@ -2,3 +2,6 @@ import "../css/style.css"
 
 import accessibility from "./modules/accessibility"
 import aslVid from "./modules/aslVid"
+import Search from "./modules/Search"
+
+const search = new Search()
