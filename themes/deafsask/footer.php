727f29a682797f66314d74abdb62f4969b9113c2
deanwiebe <wiebedean@gmail.com>
2021-05-20 15:24:29 -0600

    work on search overlay begin Search.js

--- themes/deafsask/footer.php
+++ themes/deafsask/footer.php
@@ -2,9 +2,13 @@
     <?php get_template_part('social_media'); ?>
     <a href="http://deanwiebe.ca/"><p> www.deanwiebe.ca &copy 2021</p></a>
     <div class="search-overlay">
-        <form action="">
-            <input class="search-input" type="text">
-        </form>
+        <div class="search-overlay__top">
+            <dev class="search_container">
+                <i class="fa fa-search search-overlay__icon" aria="hidden"></i>
+                <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+                <i class="fa fa-window-close search-overlay__close" aria="hidden"></i>
+            </dev>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
--- themes/deafsask/js/modules/Search.js
+++ themes/deafsask/js/modules/Search.js
@@ -1,5 +1,16 @@
 class Search {
     constructor(){
+        //1. describe and create/initiate our object
+    }
+
+    //2.events
+
+    //3. methods
+    openOverlay(){
+
+    }
+    closeOverOverlay(){
+        
     }
 }
 
