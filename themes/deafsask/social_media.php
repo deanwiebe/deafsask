03346a6b4e9bd21d5bf875dd525720cce5e8cbba
dean <wiebedean@gmail.com>
2021-05-24 09:41:09 -0600

    bannerV2 and social_mediaV2 complete

--- themes/deafsask/social_media.php
+++ themes/deafsask/social_media.php
@@ -1,4 +1,22 @@
 <div class="social_media">
+    <div id="universal-access-button" class="universal-access-button">
+            <i class="fas fa-universal-access"></i>
+    </div>
+    <div id="universal-access-overlay" class="universal-access-overlay">
+        <div class="contrast">
+            <p id="greyScaleDown"><i class="fas fa-minus"></i></p>
+            <h3>Grey Scale</h3>
+            <p id="greyScaleUp"><i class="fas fa-plus"></i></p>
+        </div>
+        <div class="contrast">
+            <p id="fontSizeDown"><i class="fas fa-minus"></i></p>
+            <h3>Font Size</h3>
+            <p id="fontSizeUp"><i class="fas fa-plus"></i></p>
+        </div>
+        <div id="reset" class="reset">
+            <p>Reset</p>
+        </div>
+    </div>
     <ul>
         <li><a href="<?php echo esc_url(site_url('/')) ?>"><i class="fas fa-home"></i></a></li>
         <li id="fa-search"><i class="fas fa-search"></i></li>
